156a9e193f7a65a4291862f2b6a65741e6e06f4f
Faisal Mohamoud <faisal.mohamoud89@gmail.com>
2022-05-16 22:27:48 +0200

    fix: linting and prettify. remove boilerplate code

---
+++ src/pages/index.tsx
@@ -0,0 +1,25 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Next.jS App</title>
+        <meta name="description" content="Next.js application" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>Hello World!</h1>
+
+        <p className={styles.description}>
+          Build something cool with this Next.js typescript starter template
+        </p>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
